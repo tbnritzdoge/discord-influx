9cf104011275e0311cd20ed8dfa965ea26a73024
doge <shadowphoenixcoc@gmail.com>
2020-09-20 22:06:13 -0400

    Update InfluxClient.js

--- src/structs/InfluxClient.js
+++ src/structs/InfluxClient.js
@@ -7,7 +7,9 @@ module.exports = class InfluxClient extends EventEmitter {
         this.influx = new Influx(...args)
     }
     start() {
-
+        this.influx.getDatabaseNames().then(names => {
+            if (!names.includes(process.env.INFLUX_DATABASE)) influx.createDatabase(process.env.INFLUX_DATABASE)
+          })
     }
     writeBotEvent(eventName, tags) {
         this.client.logger.debug(`Writing event ${eventName} ${JSON.stringify(tags)}`)
